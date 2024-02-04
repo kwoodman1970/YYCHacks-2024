1cfb0bf09321f33dc0756d291ffe5f6b46d498c0
joshlarue <44442031+joshlarue@users.noreply.github.com>
2024-02-04 11:15:11 -0700

    formated and working

--- frontend/components/ui/DashboardIcon.jsx
+++ frontend/components/ui/DashboardIcon.jsx
@@ -4,11 +4,9 @@ import '../styles/Dashboard.css'
 function DashboardIcon(props) {
   return (
     <div style={{ backgroundColor: '#C8102E', height: '12rem', width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-        <div style={{ alignItems: 'center', justifyContent: 'center', display: 'flex', height: '4rem', width: '4rem', borderRadius: '1rem', backgroundColor: 'white', marginLeft: '4rem', marginRight: '4rem'}}>
-            <div>
-              <img  src={props?.imageIcon} onClick={props.handleClick} id={props?.iconLabel} alt={props?.iconLabel + ' Icon'} style={{height: '8rem', width: '8rem'}} />
-              <span style={{fontWeight: '600', color: 'white', fontFamily: 'sans-serif', fontSize: '1.25rem'}}>{props?.iconLabel}</span>
-            </div>
+        <div style={{alignItems: 'center', justifyContent: 'center', display: 'flex', flexDirection: 'column'}}>
+          <img  src={props?.imageIcon} onClick={props.handleClick} id={props?.iconLabel} alt={props?.iconLabel + ' Icon'} style={{height: '6rem', width: '6rem'}} />
+          <span style={{fontWeight: '600', color: 'white', fontFamily: 'sans-serif', fontSize: '1.25rem', marginTop: '0.5rem'}}>{props?.iconLabel}</span>
         </div>
     </div>
   );
